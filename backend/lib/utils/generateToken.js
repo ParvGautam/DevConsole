6db351e3a1d116473af82fdd042e59666b20ce42
Parv <parv21153038@akgec.ac.in>
2025-07-25 23:33:15 +0530

    change the deoployed backend url

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -4,10 +4,10 @@ export const generateTokenAndSetCookie= (userId,res)=>{
     const token= jwt.sign({userId}, process.env.JWT_SECRET,{
         expiresIn: '15d'
     })
-    res.cookie("jwt",token,{
-        mageAge: 15*24*60*60*1000, //in ms
-        httpOnly:true,
-        sameSite:'strict',
-        secure: process.env.NODE_ENV !== 'development'
-    })
+    res.cookie("jwt", token, {
+  maxAge: 15 * 24 * 60 * 60 * 1000, // in ms
+  httpOnly: true,
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  secure: process.env.NODE_ENV === "production"
+});
 }
--- backend/server.js
+++ backend/server.js
@@ -32,8 +32,8 @@ app.use(cookieParser());
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://your-frontend-domain.netlify.app"
-  ], // Allow both local dev and deployed frontend
+    "https://devconsole-ochre.vercel.app"
+  ],
   credentials: true
 }));
 
